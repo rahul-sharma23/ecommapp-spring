53fdca5d95c87a1e465f9b94047556b2945fd13e
rahul <rahulsrmacs@gmail.com>
2022-06-18 20:41:37 +0530

    initial commit

---
+++ src/main/java/com/rahul/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.rahul.ecommerce.repository;
+
+import com.rahul.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Category findByCategoryName(String categoryName);
+
+}
